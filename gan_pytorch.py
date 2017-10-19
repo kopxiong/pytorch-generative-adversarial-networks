7526c4c3b4dbcd5adb1e57a7fae01da36a150ecc
kopxiong <kopxiong1989@gmail.com>
2017-10-19 15:39:43 +0200

    add some comments

--- gan_pytorch.py
+++ gan_pytorch.py
@@ -10,42 +10,46 @@ import torch.optim as optim
 from torch.autograd import Variable
 
 # Data params
-data_mean = 4
-data_stddev = 1.25
+data_mean       = 4
+data_stddev     = 1.25
 
 # Model params
-g_input_size = 1     # Random noise dimension coming into generator, per output vector
-g_hidden_size = 50   # Generator complexity
-g_output_size = 1    # size of generated output vector
-d_input_size = 100   # Minibatch size - cardinality of distributions
-d_hidden_size = 50   # Discriminator complexity
-d_output_size = 1    # Single dimension for 'real' vs. 'fake'
-minibatch_size = d_input_size
+g_input_size    = 1     # Random noise dimension coming into generator, per output vector
+g_hidden_size   = 50    # Generator complexity
+g_output_size   = 1     # size of generated output vector
+d_input_size    = 100   # Minibatch size - cardinality of distributions
+d_hidden_size   = 50    # Discriminator complexity
+d_output_size   = 1     # Single dimension for 'real' vs. 'fake'
+minibatch_size  = d_input_size
 
 d_learning_rate = 2e-4  # 2e-4
 g_learning_rate = 2e-4
-optim_betas = (0.9, 0.999)
-num_epochs = 30000
-print_interval = 200
-d_steps = 1  # 'k' steps in the original GAN paper. Can put the discriminator on higher training freq than generator
-g_steps = 1
+optim_betas     = (0.9, 0.999)
+num_epochs      = 30000 #30000
+print_interval  = 200
 
-# ### Uncomment only one of these
+"""
+'k' steps in the original GAN paper. Can put the discriminator
+on higher training freq than generator.
+"""
+d_steps         = 1
+g_steps         = 1
+
+# Uncomment only one of these (using anonymous functions)
 #(name, preprocess, d_input_func) = ("Raw data", lambda data: data, lambda x: x)
-(name, preprocess, d_input_func) = ("Data and variances", lambda data: decorate_with_diffs(data, 2.0), lambda x: x * 2)
+(name, preprocess, d_input_func) = ("Data and variances",
+    lambda data: decorate_with_diffs(data, 2.0), lambda x: x * 2)
 
 print("Using data [%s]" % (name))
 
-# ##### DATA: Target data and generator input data
-
+# DATA: Target data and generator input data
 def get_distribution_sampler(mu, sigma):
     return lambda n: torch.Tensor(np.random.normal(mu, sigma, (1, n)))  # Gaussian
 
 def get_generator_input_sampler():
     return lambda m, n: torch.rand(m, n)  # Uniform-dist data into generator, _NOT_ Gaussian
 
-# ##### MODELS: Generator model and discriminator model
-
+# MODELS: Generator model and discriminator model
 class Generator(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(Generator, self).__init__()
@@ -79,14 +83,20 @@ def stats(d):
 def decorate_with_diffs(data, exponent):
     mean = torch.mean(data.data, 1, keepdim=True)
     mean_broadcast = torch.mul(torch.ones(data.size()), mean.tolist()[0][0])
+    #print(type(mean), mean.size())
+    #print(type(mean_broadcast), mean_broadcast.size())
     diffs = torch.pow(data - Variable(mean_broadcast), exponent)
     return torch.cat([data, diffs], 1)
 
 d_sampler = get_distribution_sampler(data_mean, data_stddev)
 gi_sampler = get_generator_input_sampler()
 G = Generator(input_size=g_input_size, hidden_size=g_hidden_size, output_size=g_output_size)
-D = Discriminator(input_size=d_input_func(d_input_size), hidden_size=d_hidden_size, output_size=d_output_size)
-criterion = nn.BCELoss()  # Binary cross entropy: http://pytorch.org/docs/nn.html#bceloss
+D = Discriminator(input_size=d_input_func(d_input_size), hidden_size=d_hidden_size,
+    output_size=d_output_size)
+
+# Binary cross entropy: http://pytorch.org/docs/nn.html#bceloss
+criterion = nn.BCELoss()
+#criterion = nn.BCEWithLogitsLoss()
 d_optimizer = optim.Adam(D.parameters(), lr=d_learning_rate, betas=optim_betas)
 g_optimizer = optim.Adam(G.parameters(), lr=g_learning_rate, betas=optim_betas)
 
@@ -95,13 +105,13 @@ for epoch in range(num_epochs):
         # 1. Train D on real+fake
         D.zero_grad()
 
-        #  1A: Train D on real
+        # 1A: Train D on real
         d_real_data = Variable(d_sampler(d_input_size))
         d_real_decision = D(preprocess(d_real_data))
         d_real_error = criterion(d_real_decision, Variable(torch.ones(1)))  # ones = true
         d_real_error.backward() # compute/store gradients, but don't change params
 
-        #  1B: Train D on fake
+        # 1B: Train D on fake
         d_gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
         d_fake_data = G(d_gen_input).detach()  # detach to avoid training G on these labels
         d_fake_decision = D(preprocess(d_fake_data.t()))
